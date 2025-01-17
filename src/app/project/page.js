4f44485c81bc5558e401e23a3f481e4d570ea402
Govindaphuyal <91271305+Govindaphuyal@users.noreply.github.com>
2025-01-17 16:41:37 +0545

    Update page.js

--- src/app/project/page.js
+++ src/app/project/page.js
@@ -11,16 +11,16 @@ const project = () => {
       <div className="max-w-sm rounded overflow-hidden shadow-lg">
           <img className="w-full" src="/image/Screenshot 2025-01-07 130136.png" alt="Sunset in the mountains"/>
           <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <div className="font-bold text-xl mb-2">Restfull api</div>
               <p className="text-gray-700 text-base">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
                   perferendis eaque, exercitationem praesentium nihil.
               </p>
           </div>
           <div className="px-6 py-4">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Reactjs</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Nextjs</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#postgres</span>
           </div>
       </div>
   </div>
@@ -28,16 +28,16 @@ const project = () => {
       <div className="max-w-sm rounded overflow-hidden shadow-lg">
           <img className="w-full" src="/image/Screenshot 2025-01-07 130812.png" alt="Sunset in the mountains"/>
           <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <div className="font-bold text-xl mb-2">School Landing Page</div>
               <p className="text-gray-700 text-base">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
                   perferendis eaque, exercitationem praesentium nihil.
               </p>
           </div>
           <div className="px-6 py-4">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Javascript</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#React</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#htmlcss</span>
           </div>
       </div>
   </div>
@@ -45,16 +45,16 @@ const project = () => {
       <div className="max-w-sm rounded overflow-hidden shadow-lg">
           <img className="w-full" src="/image/Screenshot 2025-01-07 131754.png" alt="Sunset in the mountains"/>
           <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <div className="font-bold text-xl mb-2">react restfulApi</div>
               <p className="text-gray-700 text-base">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
                   perferendis eaque, exercitationem praesentium nihil.
               </p>
           </div>
           <div className="px-6 py-4">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Reactjs</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#javascript</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#html</span>
           </div>
       </div>
   </div>
